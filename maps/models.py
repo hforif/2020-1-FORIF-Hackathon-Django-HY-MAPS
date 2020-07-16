6f64aea95d9d30f4a43ce3c6de750f6a16255ad8
Neulhan <sting7675@naver.com>
2020-07-16 21:13:03 +0900

    create user model, start intro page

--- maps/models.py
+++ maps/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Building(models.Model):
+    number = models.IntegerField(primary_key=True)
+    cv_store = models.TextField(default="", blank=True)
+    cafe = models.TextField(default="", blank=True)
+    lounge = models.TextField(default="", blank=True)
+    portal = models.TextField(default="", blank=True)
+    department = models.TextField(default="", blank=True)
+    one_line = models.TextField(default="", blank=True)
+    cafeteria = models.TextField(default="", blank=True)
